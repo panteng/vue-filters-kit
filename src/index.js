11287973ab7ea47f7585e7b9fc521ea9f20be3ff
Chris Pan <chrispan.pt@gmail.com>
2016-03-03 09:52:24 +0800

    Setup app and router.

---
+++ src/index.js
@@ -0,0 +1,39 @@
+var Vue = require('vue');
+var VueRouter = require('vue-router');
+
+// vue config
+Vue.use(VueRouter);
+Vue.config.debug = true;
+
+// register filters
+Vue.filter('booleanFormatter', require('./filters/booleanFormatter'));
+Vue.filter('percentageFormatter', require('./filters/percentageFormatter'));
+Vue.filter('sizeFormatter', require('./filters/sizeFormatter'));
+Vue.filter('timeFormatter', require('./filters/timeFormatter'));
+
+// register views
+Vue.component('v-demos', require('./views/demos'));
+
+// app setup
+var App = Vue.extend({
+    template: require('./template.html'),
+    replace: false
+});
+
+// router setup
+var Router = new VueRouter({
+    history: false,
+    linkActiveClass: 'active'
+});
+
+Router.map({
+    '/': {
+        component: Vue.component('v-demos')
+    }
+});
+
+Router.redirect({
+    '*': '/'
+});
+
+Router.start(App, '#app');
